e338351eef32807d3aeb94a6ad825e3f3ef6a6f0
fuxm00 <fuxm00@vse.cz>
2022-04-04 12:12:33 +0200

    Barel + prefaby
    Vytvoření barelu a potřebných prefabů

---
+++ My project/Assets/Barell.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barell : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float randomRotation = Random.Range(-90, 90);
+        transform.Rotate(0, 0, randomRotation);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //TODO efekt výbuchu
+        StartCoroutine(Explode());
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(this.gameObject);
+    }
+
+    IEnumerator Explode()
+    {
+        float randomSeconds = Random.Range(2, 4);
+        yield return new WaitForSeconds(randomSeconds);
+        Destroy(gameObject);
+    }
+}
